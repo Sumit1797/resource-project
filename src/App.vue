ae635635dfa40da27c48cc0dab47f6ff7582e77d
Sumit Suresh Poojary <sumit97poojary@gmail.com>
2024-04-21 23:51:50 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <h1>Hello World</h1>
+    <ul>
+        <resource-item v-for="res in storedresources" 
+        :key="res.id"
+        :title="res.title"
+        :description="res.description"
+        :link="res.link"></resource-item>
+    </ul>
+</template>
+
+<script>
+import ResourceItem from './components/ResourceItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ResourceItem
+  },
+  data () {
+    return {
+      storedresources: [
+        {
+          id: 'offciial-1', 
+          title: 'this is an initial title',
+          description: 'a short description',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'offciial-2', 
+          title: 'this is an initial title',
+          description: 'a short description for 2nd course',
+          link: 'https://google.com'
+        }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/ResourceItem.vue
@@ -0,0 +1,40 @@
+<template>
+   <li>
+        <div>
+            <header>
+                <h3>{{ title }}</h3>
+                <button>Delete</button>
+            </header>
+        </div>
+        <p>
+            {{ description }}
+        </p>
+        <nav>
+            <a :href="link">View resource</a>
+        </nav>
+    </li>
+</template>
+
+<script>
+export default {
+    props:['title', 'description', 'link']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
